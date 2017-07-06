a7a94af41cc2da0b6df2f9be3d33cd0b1ce18df1
Pierre-Alexandre St-Jean <pa@stjean.me>
2017-07-06 14:06:21 -0400

    Typo

--- install_and_start_perfmon.sh
+++ install_and_start_perfmon.sh
@@ -7,7 +7,7 @@ sh install_cht_perfmon.sh 14 $CLOUD_HEALTH_SECRET aws;
 
 # modify the ec2 file, restart process
 echo "Restarting perfmon with hacked ec2 things"
-cp /ec2_hack.rb /opt/cht_perfmon/embedded/lib/ruby/gems/2.1.0/gems/facter-2.4.6/lib/facter/ec2.rbà
+cp /ec2_hack.rb /opt/cht_perfmon/embedded/lib/ruby/gems/2.1.0/gems/facter-2.4.6/lib/facter/ec2.rb
 sudo /etc/init.d/cht_perfmon restart 2>&1 | tee -a /tmp/agent_install_log.txt
 
 echo "Cloud health client installed and running in the background"
